71d1b11b41664861f15ff49fe7a18d5ab3bf7185
Albert Agram <albert.agram@gmail.com>
2015-05-14 17:22:00 +0000

    indent using spaces

--- sorting and searching/rabinkarp_search.py
+++ sorting and searching/rabinkarp_search.py
@@ -6,17 +6,17 @@ of the strings
 """
 
 def search(string, sub):
-	string_legnth, sub_length = len(string), len(sub)
-	hsub_digest = md5(sub).digest()
-	offsets = []
+    string_legnth, sub_length = len(string), len(sub)
+    hsub_digest = md5(sub).digest()
+    offsets = []
 
-	if string_legnth > sub_length:
-		return offsets
+    if string_legnth > sub_length:
+        return offsets
 
 
-	for i in xrange(string_legnth - sub_length + 1):
-		if md5(string[i:i + sub_length]).digest() == hsub_digest:
-			if string[i:i + sub_length] == sub:
-				offsets.append(i)
+    for i in xrange(string_legnth - sub_length + 1):
+        if md5(string[i:i + sub_length]).digest() == hsub_digest:
+            if string[i:i + sub_length] == sub:
+                offsets.append(i)
 
-	return offsets
+    return offsets
