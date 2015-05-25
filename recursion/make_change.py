9c9357485d3c00ffc77f9794a60c5e8f6aa6ed9d
Albert Agram <albert.agram@gmail.com>
2015-05-25 06:58:39 +0000

    modify make_change code

--- recursion/make_change.py
+++ recursion/make_change.py
@@ -1,11 +1,10 @@
-def dpMakeChange(coinValueList,change,minCoins,coinsUsed):
+def dp_make_change(coin_value_list, change, min_coins, coins_used):
    for cents in range(change+1):
-      coinCount = cents
-      newCoin = 1
-      for j in [c for c in coinValueList if c <= cents]:
-            if minCoins[cents-j] + 1 < coinCount:
-               coinCount = minCoins[cents-j]+1
-               newCoin = j
-      minCoins[cents] = coinCount
-      coinsUsed[cents] = newCoin
-   return minCoins[change]
+      coin_count = cents
+      for j in [c for c in coin_value_list if c <= cents]:
+            if min_coins[cents-j] + 1 < coin_count:
+               coin_count = min_coins[cents-j]+1
+               new_coin = j
+      min_coins[cents] = coin_count
+      coins_used[cents] = new_coin
+   return min_coins[change]
