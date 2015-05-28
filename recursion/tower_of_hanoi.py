b9411f2ad475291f6eb86e6e5b37755ddda9e3db
Albert Agram <albert.agram@gmail.com>
2015-05-28 09:52:47 +0000

    add notes to tower of hanoi problem

--- recursion/tower_of_hanoi.py
+++ recursion/tower_of_hanoi.py
@@ -1,3 +1,11 @@
+"""
+Here is a high-level outline of how to move a tower from the starting pole, to the goal pole, using an intermediate pole:
+
+1) Move a tower of height-1 to an intermediate pole, using the final pole.
+2) Move the remaining disk to the final pole.
+3) Move the tower of height-1 from the intermediate pole to the final pole using the original pole.
+"""
+
 def move_tower(height, from_pole, to_pole, with_pole):
     if height >= 1:
         move_tower(height - 1, from_pole, with_pole, to_pole)
