733cfd5629e3a5878592b3a486e5be2946f9bc14
Albert Agram <albert.agram@gmail.com>
2015-07-08 16:17:13 +0000

    iterative inorder

--- trees_and_tree_algorithms/binary_tree.py
+++ trees_and_tree_algorithms/binary_tree.py
@@ -111,4 +111,22 @@ def iterative_post_order(root):
     while s2 != []:
         root = s2.pop()
         print root.key
+
+# O(n) time O(n) space
+def iterative_inorder(root):
+    if root == None:
+        return
+
+    stack = []
+
+    while True:
+        if root != None:
+            stack.append(root)
+            root = root.left_child
+        else:
+            if stack == []:
+                break
+            root = stack.pop()
+            print root.key
+            root = root.right_child
 # print tree.post_order()
