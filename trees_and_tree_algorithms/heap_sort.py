ba49ca0655af90fe6ece509c0225b210c411fa09
Albert Agram <albert.agram@gmail.com>
2015-07-22 16:36:44 +0000

    modified heap sort

--- trees_and_tree_algorithms/heap_sort.py
+++ trees_and_tree_algorithms/heap_sort.py
@@ -1,9 +1,14 @@
-from binary_heap_min_heap import BinHeap
+from binary_heap_min_heap import BinaryHeap
 
 def heap_sort(a_list):
-    min_heap = BinHeap()
+    sorted_array = []
+    min_heap = BinaryHeap()
     min_heap.build_heap(a_list)
-    return min_heap.heap_list[1:]
+
+    while min_heap.current_size > 0:
+        sorted_array.append(min_heap.del_min())
+
+    return sorted_array
 
 
-print heap_sort([9, 6, 5, 2, 3])
+print heap_sort([1, 3, 5, 7, 2, 4, 6, 8, 0, 9, 10, 11])
