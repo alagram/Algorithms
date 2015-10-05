1909c612ab3bf9c0229ea67ec1f70768059fa8ea
Albert Agram <albert.agram@gmail.com>
2015-10-05 10:52:34 +0000

    queue, stack, deque, sll ordered list

---
+++ ruby/data_structures/stack.rb
@@ -0,0 +1,44 @@
+class Stack
+  attr_accessor :items
+
+  def initialize
+    @items = []
+  end
+
+  def is_empty
+    items == []
+  end
+
+  def push(item)
+    items.push(item)
+  end
+
+  def pop
+    return nil if items.empty?
+    return items.pop
+  end
+
+  def peek
+    return nil if items.empty?
+    return items[-1]
+  end
+
+  def size
+    items.length
+  end
+end
+
+
+
+s = Stack.new
+p s.is_empty
+s.push(1)
+s.push(2)
+s.push(3)
+s.push(4)
+
+p s.peek
+p s.pop
+p s.peek
+
+p s.is_empty
